f88a68dfaaa71f771f81b277eb64e7f8cb169f38
Manish <itzmanish108@gmail.com>
2019-08-26 11:15:52 +0530

    update travis config and test

--- account/tests.py
+++ account/tests.py
@@ -12,22 +12,31 @@ UserModel = get_user_model()
 class LoginViewTest(APITestCase):
     url = reverse('login')
 
+    def setUp(self):
+        UserModel.objects.create_user(
+            username='rudra', email='rudra@gmal.com', password='rudra@108')
+
+    def test_created_user(self):
+        user = UserModel.objects.filter(username='rudra')
+        self.assertEquals(user.count(), 1)
+
     def test_login_failed(self):
         data = {
             'username': 'rudra',
             'password': 'pass123'
         }
-        res = self.client.post(self.url, data, format='json')
+        res = self.client.post(self.url, data=data, format='json')
+        print(res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_login_success(self):
-    #     data = {
-    #         'username': 'rudr',
-    #         'password': 'rudra@108',
-    #     }
-    #     res = self.client.post(self.url, data=data, format='json')
-    #     print(res.data)
-    #     self.assertEqual(res.status_code, status.HTTP_200_OK)
+    def test_login_success(self):
+        data = {
+            'username': 'rudr',
+            'password': 'rudra@108',
+        }
+        res = self.client.post(self.url, data=data)
+        print(res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
 
 class RegisterViewTest(APITestCase):
