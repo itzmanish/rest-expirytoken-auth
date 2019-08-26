f6eb180f10e6d55686c649511d6e3bd7f0db4236
Manish <itzmanish108@gmail.com>
2019-08-26 10:18:26 +0530

    added names on url for reversing using name Added test

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from django.urls import reverse
+
+
+class LoginViewTest(APITestCase):
+    url = reverse('login')
+
+    def test_login_failed(self):
+        data = {
+            'username': 'rudra',
+            'password': 'pass123'
+        }
+        res = self.client.post(self.url, data, format='json')
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login_success(self):
+        data = {
+            'username': 'rudra',
+            'password': 'rudra@108',
+        }
+        res = self.client.post(self.url, data=data, format='json')
+        print(dir(res))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
--- account/urls.py
+++ account/urls.py
@@ -7,9 +7,9 @@ from rest_registration.api.views import (send_reset_password_link,
                                          )
 
 urlpatterns = [
-    path('login/', LoginView),
-    path('logout/', LogoutView),
-    path('register/', register),
+    path('login/', LoginView, name='login'),
+    path('logout/', LogoutView, name='logout'),
+    path('register/', register, name='register'),
     path('verify-account/', verify_registration),
     path('reset/password/', reset_password),
     path('reset-password-link/', send_reset_password_link)
